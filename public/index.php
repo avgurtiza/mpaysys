e9edcfc3f21a896a69e70740130de708aeccea48
Slide Gurtiza <slide.gurtiza@gmail.com>
2015-10-03 19:58:24 +0800

    Fixed bug where there are duplicate legal holiday entries

--- public/index.php
+++ public/index.php
@@ -66,7 +66,8 @@ set_include_path(implode(PATH_SEPARATOR, array(
 )));
 
 /** Zend_Application */
-require_once 'Zend/Application.php';
+require_once '../vendor/autoload.php';
+// require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
