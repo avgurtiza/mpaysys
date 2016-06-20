5d19b2e5a595c898c2ebe6c3a7b71ac1a740537b
Slide Gurtiza <slide.gurtiza@gmail.com>
2016-06-20 16:55:48 +0800

    Fixes calc where days fore and aft legal holidays are also made legal

--- library/Messervelib/Payroll.php
+++ library/Messervelib/Payroll.php
@@ -686,6 +686,18 @@ class Messervelib_Payroll
             }
 
 
+
+
+            if ($holiday_today && $holiday_today->getType() == 'special') {
+                $time_array += array('spec' => $reg, 'spec_nd' => $nd, 'spec_ot' => $ot, 'spec_nd_ot' => $nd_ot);
+            }
+
+            if ($holiday_tomorrow && $holiday_tomorrow->getType() == 'special') {
+                $time_array += array('spec' => $tomorrow, 'spec_nd' => $tomorrow_nd, 'spec_ot' => $tomorrow_ot
+                , 'spec_nd_ot' => $tomorrow_nd_ot
+                );
+            }
+
             if ($holiday_today && $holiday_today->getType() == 'legal') {
                 if (($tomorrow + $tomorrow_nd + $tomorrow_ot + $tomorrow_nd_ot) > 0) { // Attendance tomorrow?  Make it legal
                     $time_array = array_merge($time_array, array(
@@ -706,18 +718,8 @@ class Messervelib_Payroll
                 }
             }
 
-            if ($holiday_today && $holiday_today->getType() == 'special') {
-                $time_array += array('spec' => $reg, 'spec_nd' => $nd, 'spec_ot' => $ot, 'spec_nd_ot' => $nd_ot);
-            }
-
-            if ($holiday_tomorrow && $holiday_tomorrow->getType() == 'special') {
-                $time_array += array('spec' => $tomorrow, 'spec_nd' => $tomorrow_nd, 'spec_ot' => $tomorrow_ot
-                , 'spec_nd_ot' => $tomorrow_nd_ot
-                );
-            }
-
             if ($holiday_tomorrow && $holiday_tomorrow->getType() == 'legal') {
-                if (($reg + $nd + $ot + $nd_ot) > 0) { // Attendance yesterday?  Make it legal
+                if (($tomorrow + $tomorrow_nd + $tomorrow_ot + $tomorrow_nd_ot) > 0) { // Attendance yesterday?  Make it legal
                     $time_array = array_merge($time_array, array(
                         'legal' => $reg + $tomorrow
                         , 'legal_nd' => $nd + $tomorrow_nd
