b951b847184c838997a44908240d570f43f565a8
Slide Gurtiza <slide@teleserv.ph>
2017-01-27 08:35:46 +0800

    Adds calamity loan deductions

--- application/forms/EditDeduction.php
+++ application/forms/EditDeduction.php
@@ -12,54 +12,59 @@ class Messerve_Form_EditDeduction extends Zend_Form
     public function init()
     {
         $this->setMethod('post');
-        
-        $this->setOptions(array('id'=>'edit-deduction'));
+
+        $this->setOptions(array('id' => 'edit-deduction'));
 
         $this->addElement(
-        		$this->createElement('text', 'amount')
-        		->setLabel('Amount')
-        		->setRequired(true)
-        		->addValidator(new Zend_Validate_Float())
-        		->addFilter(new Zend_Filter_StringTrim())
+            $this->createElement('text', 'amount')
+                ->setLabel('Amount')
+                ->setRequired(true)
+                ->addValidator(new Zend_Validate_Float())
+                ->addFilter(new Zend_Filter_StringTrim())
         );
-        
+
+        $deduction_types = [
+            'sss_loan' => 'SSS loan'
+            , 'hdmf_loan' => 'HDMF loan'
+            , 'fuel_overage' => 'Fuel overage'
+            , 'maintenance_deduct' => 'Maintenance'
+            , 'accident' => 'Accident'
+            , 'uniform' => 'Uniform'
+            , 'lost_card' => 'Lost fleet card'
+            , 'food' => 'Food'
+            , 'communication' => 'Communication'
+            , 'adjustment' => 'Adjustment'
+            , 'misc' => 'Miscellaneous'
+            , 'sss_calamity' => 'SSS calamity'
+            , 'hdmf_calamity' => 'HDMF calamity'
+
+        ];
+
+        asort($deduction_types);
         $this->addElement(
-        		$this->createElement('select', 'type')
-        		->setLabel('Type')
-        		->setMultiOptions(array(
-        					'sss_loan'=>'SSS loan'
-        					,'hdmf_loan' => 'HDMF loan'
-        					,'fuel_overage' => 'Fuel overage'
-        					,'maintenance_deduct' => 'Maintenance'
-        					,'accident' => 'Accident'
-        					,'uniform' => 'Uniform'
-        					,'lost_card' => 'Lost fleet card'
-        					,'food' => 'Food'
-        					,'communication' => 'Communication'
-        					,'adjustment' => 'Adjustment'
-        					,'misc' => 'Miscellaneous'
-        				
-        				))
+            $this->createElement('select', 'type')
+                ->setLabel('Type')
+                ->setMultiOptions($deduction_types)
         );
-        
+
         $this->addElement(
-        		$this->createElement('text', 'deduction')
-        		->setLabel('Deduction per cutoff')
-        		->setRequired(true)
-        		->addValidator(new Zend_Validate_Float())
-        		->addFilter(new Zend_Filter_StringTrim())
+            $this->createElement('text', 'deduction')
+                ->setLabel('Deduction per cutoff')
+                ->setRequired(true)
+                ->addValidator(new Zend_Validate_Float())
+                ->addFilter(new Zend_Filter_StringTrim())
         );
-                
+
         $this->addElement(
-        		$this->createElement('select', 'cutoff')
-        		->setLabel('Cut-off')
-        		->setMultiOptions(array(
-        				'1'=>'1-15'
-        				,'2'=>'16-31'
-        				, '3'=>'Both'
-        		))
+            $this->createElement('select', 'cutoff')
+                ->setLabel('Cut-off')
+                ->setMultiOptions(array(
+                    '1' => '1-15'
+                , '2' => '16-31'
+                , '3' => 'Both'
+                ))
         );
-        
+
         /*
 		$this->addElement(
 				$this->createElement('text', 'balance')
@@ -69,23 +74,23 @@ class Messerve_Form_EditDeduction extends Zend_Form
 				->addFilter(new Zend_Filter_StringTrim())
 		);
 		*/
-        
-		$this->addDisplayGroup(array(
-				'amount'
-				, 'type'
-				, 'deduction'
-				, 'cutoff'
-				// , 'balance'
-		) , 'deductions', array('legend'=>'Add new deduction schedule
+
+        $this->addDisplayGroup(array(
+            'amount'
+        , 'type'
+        , 'deduction'
+        , 'cutoff'
+            // , 'balance'
+        ), 'deductions', array('legend' => 'Add new deduction schedule
 				'));
-		
-		$this->addElement(
-				$this->createElement('button', 'submit')
-				->setLabel('Save')
-				->setAttrib('type', 'submit')
-		);
-		
-		
-		parent::init();
+
+        $this->addElement(
+            $this->createElement('button', 'submit')
+                ->setLabel('Save')
+                ->setAttrib('type', 'submit')
+        );
+
+
+        parent::init();
     }
 }
