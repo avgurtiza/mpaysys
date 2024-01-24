5c2e72a54563aa003943ddecbb7d762e7b21d862
Slide Gurtiza <slide.gurtiza@voyager.nz>
2024-01-24 08:46:25 +0800

    Fixes issue where non-vat clients are billed with vat

--- application/modules/payroll/controllers/IndexController.php
+++ application/modules/payroll/controllers/IndexController.php
@@ -3455,7 +3455,7 @@ class Payroll_IndexController extends Zend_Controller_Action
                 $page->setFont($mono, $bill_font_size)->drawText(str_pad(number_format($value, 2), 12, ' ', STR_PAD_LEFT), $dim_x + 280, $dim_y);
                 $total_amount += round($value, 2);
 
-                if (!$Group->getNonVat() > 0) {
+                if ($Group->getNonVat() == 1) {
                     $vat_net = $value / 1.12;
 
                     $page->setFont($mono, $bill_font_size)->drawText(str_pad(number_format($vat_net, 2), 12, ' ', STR_PAD_LEFT)
@@ -3480,7 +3480,7 @@ class Payroll_IndexController extends Zend_Controller_Action
         $vat_net = $total_amount / 1.12;
 
 
-        if (!$Group->getNonVat() > 0) {
+        if ($Group->getNonVat() == 1) {
             $page->setFont($mono_bold, $bill_font_size)->drawText(str_pad(number_format($vat_net, 2), 12, ' ', STR_PAD_LEFT)
                 , $dim_x + 355, $dim_y);
 
@@ -3492,7 +3492,7 @@ class Payroll_IndexController extends Zend_Controller_Action
 
         $page->setFont($font, $bill_font_size)->drawText('VATABLE SALES', $dim_x, $dim_y);
 
-        if (!$Group->getNonVat() > 0) {
+        if ($Group->getNonVat() == 1) {
             $page->setFont($mono, $bill_font_size)->drawText(str_pad(number_format($vat_net, 2), 12, ' ', STR_PAD_LEFT), $dim_x + 285, $dim_y);
         }
 
@@ -3501,7 +3501,7 @@ class Payroll_IndexController extends Zend_Controller_Action
         $page->setFont($font, $bill_font_size)->drawText('VALUE ADDED TAX (VAT)', $dim_x, $dim_y);
         $page->setFont($bold, $bill_font_size)->drawText('12%', $dim_x + 185, $dim_y);
 
-        if (!$Group->getNonVat() > 0) {
+        if ($Group->getNonVat() == 1) {
             $page->setFont($mono, $bill_font_size)->drawText(str_pad(number_format($vat_net * 0.12, 2), 12, ' ', STR_PAD_LEFT), $dim_x + 285, $dim_y);
         }
 
